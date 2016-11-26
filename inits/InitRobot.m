c6719a58c604639b1a8a9959f743923662e99117
ByungSoo Ko <kobiso62@gmail.com>
2016-11-26 15:40:59 +0900

    - Make the initial pose of robot randomized

--- inits/InitRobot.m
+++ inits/InitRobot.m
@@ -5,9 +5,9 @@
 %   robot.laseAngles - angles of the lasers; [rightmost to leftmost]
 %   robot.goal = the goal! [x, y, goal radius]
 
-%initialize robot
+%initialize robot, make the initial pose randomized
 G=randi([-900,900], 1,2)./100;
-robot  = struct('pose', [9;-9; 3*pi/4] ,'param',[5; 2; 2], 'size', [1.5, 1], 'laserAngles', -sensor_ang:2*sensor_ang/(sensor_num - 1):sensor_ang, 'goal',[-7.25; 7.6; 0.3]);
+robot  = struct('pose', [8+(10-8)*rand(1,1);(-10)+((-8)-(-10))*rand(1,1); 3*pi/4] ,'param',[5; 2; 2], 'size', [1.5, 1], 'laserAngles', -sensor_ang:2*sensor_ang/(sensor_num - 1):sensor_ang, 'goal',[-7.25; 7.6; 0.3]);
 
 %initialize history
 poseHist = [];
